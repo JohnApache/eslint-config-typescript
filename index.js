9d251b88019f7bef788b3316658838a5e097e100
cjw <1468733552@qq.com>
2019-12-24 14:56:19 +0800

    Init eslint-config-typescript

---
+++ index.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6    : true,
+        node   : true,
+        mocha  : true,
+    },
+    globals: {
+        Atomics          : 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType : 'module',
+    },
+    plugins: [
+        'import',
+        'promise',
+        '@typescript-eslint',
+    ],
+    'extends': [
+        './src/base.js',
+        './src/es6.js',
+        './src/node.js',
+        './src/promise.js',
+        './src/import.js',
+        './src/typescript.js',
+    ].map(require.resolve),
+    rules: {},
+};
